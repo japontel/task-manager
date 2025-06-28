73bbeec3622dffb0febf88b7ab77c1c5ecf2db93
joseaponteholaamigo <jose.aponte@holaamigo.ai>
2025-06-27 23:32:12 -0500

    feat(categories): implement CRUD functionality for categories with Inertia.js. Improve routes for Tasks

---
+++ resources/js/Pages/Categories/Edit.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3'
+
+const props = defineProps({ category: Object })
+
+const form = useForm({
+  name: props.category.name
+})
+
+const submit = () => {
+  form.put(route('categories.update', props.category.id))
+}
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-xl font-bold mb-4">Create Category</h1>
+
+    <form @submit.prevent="submit">
+      <label class="block mb-2">Name</label>
+      <input v-model="form.name" type="text" class="border rounded p-2 w-full mb-4" />
+      <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Save</button>
+    </form>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,8 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::resource('categories', CategoryController::class)->except(['show']);
+    Route::resource('tasks', TaskController::class)->except(['show']);
 });
 
 require __DIR__.'/auth.php';
